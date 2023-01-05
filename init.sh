db9d97d70749fa9a1b7ce048b6cd2c2469546176
Dmitriy Kuzmenko <79150928+dsk13-stack@users.noreply.github.com>
2023-01-05 17:43:36 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,8 +1,7 @@
-mkdir -p ./web/{public,uploads,etc} && mkdir ./web/public/{img,css,js}
-cp hello.py ./web/hello.py
-cp nginx.conf ./web/etc/nginx.conf
+mkdir -p ~/web/{public,uploads,etc}&&mkdir ~/web/public/{img,css,js}
+cp hello.py ~/web/hello.py
+cp nginx.conf ~/web/etc/nginx.conf
 rm -r /etc/nginx/sites-enabled/default
 ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 nginx -t && nginx restart
-cd ./web
-gunicorn -b :8080 hello:wsgi_app
+gunicorn -b 0.0.0.0:8080 -c ~/web/python.py hello:wsgi_app
