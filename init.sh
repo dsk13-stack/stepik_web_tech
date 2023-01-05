0a041f24adf69267666b96c88cbb5d3ca240530d
Dmitriy Kuzmenko <79150928+dsk13-stack@users.noreply.github.com>
2023-01-05 17:17:27 +0300

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,8 @@
+mkdir -p ./web/{public,uploads,etc} && mkdir ./web/public/{img,css,js}
+cp hello.py ./web/hello.py
+cp nginx.conf ./web/etc/nginx.conf
+rm -r /etc/nginx/sites-enabled/default
+ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+nginx -t && nginx restart
+cd ./web
+gunicorn -b :8080 hello:app
