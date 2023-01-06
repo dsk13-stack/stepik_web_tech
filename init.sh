4ec06c80478faeed7b549189807795bf42825e00
dsk13-stack <dsk13@inbox.ru>
2023-01-06 14:14:39 +0300

    ini

--- init.sh
+++ init.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} \
-&& cp hello.py ~/web/hello.py \
 && cp nginx.conf ~/web/etc/nginx.conf \
 && rm -r /etc/nginx/sites-enabled/default \
 && ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf \
 && nginx -t && /etc/init.d/nginx restart \
-&& gunicorn -b 0.0.0.0:8080 -c ~/web/hello.py hello:wsgi_app 
+
