c35bc2012d2e299b9324c4a30c7961cd077cb009
dsk13-stack <dsk13@inbox.ru>
2023-01-06 10:49:42 +0300

    pyt

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
-def wsgi_app(environ=dict, start_response=callable) -> str:
-    response = "\r\n".join(environ["QUERY_STRING"].strip().split("&"))
+def wsgi_app(environ, start_response):
+    response = "\r\n".join(environ["QUERY_STRING"].split("&"))
     status = "200 OK"
     response_headers = [("Content-type", "text/plain"), ("Content-length", str(len(response)))]
     start_response(status, response_headers)
