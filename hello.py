31c2e1623c849ac5359c739d089f93d34d58aa4c
dsk13-stack <dsk13@inbox.ru>
2023-01-06 14:11:40 +0300

    new test

--- hello.py
+++
@@ -1,9 +0,0 @@
-def wsgi_app(environ, start_response):
-    response = "\r\n".join(environ["QUERY_STRING"].split("&"))
-    status = "200 OK"
-    response_headers = [("Content-type", "text/plain"), ("Content-length", str(len(response)))]
-    start_response(status, response_headers)
-    return [bytes(response, encoding="UTF-8")]
-
-if __name__ == "__main__":
-    print("wcgi app")
