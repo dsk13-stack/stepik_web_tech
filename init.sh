3e133325a47af9e3c4579d3162e5f6b1fb320664
dsk13-stack <dsk13@inbox.ru>
2023-01-06 15:22:08 +0300

    init

--- init.sh
+++ init.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} \
+mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} && mkdir ~/web/ask \
 && cp nginx.conf ~/web/etc/nginx.conf \
 && rm -r /etc/nginx/sites-enabled/default \
 && ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf \
 && nginx -t && /etc/init.d/nginx restart \
-
+django-admin startproject ask ~/web/ask
+~/web/ask/manage.py startapp qa
+rm -r ~/web/ask/qa/views.py
+cp views.py ~/web/ask/qa/views.py
+rm -r ~/web/ask/ask/urls.py
+cp urls.py ~/web/ask/ask/urls.py
+sed -i 's/ALLOWED_HOSTS = []/ALLOWED_HOSTS = ['*']/' ~/web/ask/ask/settings.py
+gunicorn -b 0.0.0.0:8000 -c ~/web/ask/ask/wsgi.py wsgi:application
