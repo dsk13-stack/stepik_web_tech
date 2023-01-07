a3f38469cec90444a8245f74207e7223d23ca9e8
dsk13-stack <dsk13@inbox.ru>
2023-01-07 08:57:41 +0300

    init

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} && mkdir -p ~/web/ask/qa
+mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} && mkdir -p ~/web/ask/qa \
 && cp nginx.conf ~/web/etc/nginx.conf \
 && rm -r /etc/nginx/sites-enabled/default \
 && ln -s ~/web/etc/nginx.conf /etc/nginx/sites-enabled/nginx.conf \
@@ -8,7 +8,7 @@ mkdir -p ~/web/{public,uploads,etc} && mkdir ~/web/public/{img,css,js} && mkdir 
 django-admin startproject ask ~/web/ask
 ~/web/ask/manage.py startapp qa ~/web/ask/qa
 rm -r ~/web/ask/qa/views.py
-cp views.py ~/web/ask/qa/views.py
+cp views.py ~/web/ask/qa/views.pyS
 rm -r ~/web/ask/ask/urls.py
 cp urls.py ~/web/ask/ask/urls.py
 sed -i "s/.*ALLOWED_HOSTS = .*/ALLOWED_HOSTS = ['*']/" ~/web/ask/ask/settings.py
